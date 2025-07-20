7c2665ba49e0c32d2c7eb19ee9dc2e93fee72ba1
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-07-20 14:48:12 +0200

    chore: update release env var name

--- components/release-info.tsx
+++ components/release-info.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import type { HTMLAttributes } from "react";
 
 export default function ReleaseInfo({ className, ...props }: HTMLAttributes<HTMLSpanElement>) {
-    const buildNumber = process.env.RELEASE_NUMBER || "v0.0.0-not-working";
+    const buildNumber = process.env.RELEASE_NAME || "v0.0.0-not-working";
 
     return (
         <span className={cn("text-sm leading-none", className)} {...props}>

dc8632b505b1bf7e66f0cd4d6b4b352737f4e66f
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-08-02 00:39:16 +0200

    chore(nav): add scroll navigation test

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ export default function Home() {
             <div className="font-bricolage-grotesque text-4xl font-medium">speezy</div>
             <ReleaseInfo />
             <ModeToggle />
+            <div className="h-[1000px]">test</div>
+            <div className="bg-primary text-primary-foreground">end test</div>
         </>
     );
 }

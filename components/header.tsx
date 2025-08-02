14422ea8b7a8ed6c059cd00ef76807f4ca005157
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-08-02 14:34:32 +0200

    style: increase header spacing

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Header() {
     return (
-        <header className="flex flex-row items-center justify-between py-3">
+        <header className="flex flex-row items-center justify-between pt-3 pb-6">
             <h1 className="font-bricolage-grotesque text-4xl font-medium">speezy</h1>
             <ModeToggle />
         </header>

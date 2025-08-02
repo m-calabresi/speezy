a10f7bacf4466eeeceb318d89c2136b95c2a3f76
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-08-02 12:09:13 +0200

    style(nav): update release info banner style

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -15,27 +15,26 @@ export default function Navigation() {
     const pathName = usePathname();
 
     return (
-        <nav className="bg-background absolute bottom-0 z-40 w-full">
-            <div className="mx-auto flex max-w-md items-center justify-around">
+        <nav className="bg-background absolute bottom-0 z-40 w-full py-3">
+            <ul className="mx-auto flex max-w-md items-center justify-around">
                 {navItems.map(({ id, url, label, Icon }) => {
                     const isActive = pathName === url;
                     return (
-                        <Link
-                            key={id}
-                            href={url}
-                            className={cn("text-secondary-foreground group flex min-w-[60px] flex-col items-center justify-center rounded-lg px-5 py-3", isActive && "text-muted-foreground")}>
-                            <div
-                                className={cn(
-                                    "group-hover:bg-secondary mb-2 flex w-16 items-center justify-center rounded-full py-1.5 transition-colors duration-100 ease-in-out",
-                                    isActive && "bg-secondary",
-                                )}>
-                                <Icon className="h-5 w-5" />
-                            </div>
-                            <span className={cn("text-xs font-medium", isActive && "")}>{label}</span>
-                        </Link>
+                        <li key={id} className={cn("text-secondary-foreground group px-5", isActive && "text-muted-foreground")}>
+                            <Link href={url} className="flex min-w-[60px] flex-col items-center justify-center">
+                                <div
+                                    className={cn(
+                                        "group-hover:bg-secondary mb-2 flex w-16 items-center justify-center rounded-full py-1.5 transition-colors duration-100 ease-in-out",
+                                        isActive && "bg-secondary",
+                                    )}>
+                                    <Icon className="h-5 w-5" />
+                                </div>
+                                <span className={cn("text-xs font-medium", isActive && "")}>{label}</span>
+                            </Link>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
         </nav>
     );
 }
--- components/release-info.tsx
+++ components/release-info.tsx
@@ -7,8 +7,8 @@ const buildNumber = process.env.RELEASE_NAME || "v0.0.0-development";
 
 export default function ReleaseInfo({ className, ...props }: HTMLAttributes<HTMLSpanElement>) {
     return (
-        <span className={cn("text-sm leading-none", className)} {...props}>
-            Release {buildNumber}
-        </span>
+        <h4 className={cn("bg-primary text-primary-foreground rounded-md px-2 py-1 text-sm leading-none", className)} {...props}>
+            Release <span className="font-mono">{buildNumber}</span>
+        </h4>
     );
 }

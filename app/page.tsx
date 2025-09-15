310a38c815c81d83ee75de735704f29c457d4d6f
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-09-15 09:02:22 +0200

    test: add test message

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { NewTransactionForm } from "@/components/new-transaction-form";
 import { PageProvider } from "@/providers/page-provider";
-
+// TODO: remove this comment
 export const metadata: Metadata = {
     title: "New entry - speezy",
     description: "Add a new transaction to your expense tracking.",

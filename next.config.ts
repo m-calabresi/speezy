34e54f40f78d35985c048317e13b3a6b864e5c03
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-11-08 11:51:52 +0100

    chore!: update react compiler config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
-    experimental: {
-        reactCompiler: true,
-    },
+    reactCompiler: true,
     allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "http://192.168.1.200:3000"],
 };
 

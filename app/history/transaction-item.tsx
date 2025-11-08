8aef38e2c58d670b1061f1af71d286eddb15f333
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-11-08 12:37:25 +0100

    feat: add transaction edit button

--- app/history/transaction-item.tsx
+++ app/history/transaction-item.tsx
@@ -1,6 +1,7 @@
-import { CalendarIcon, CheckIcon, CircleIcon, CoinsIcon, EuroIcon, HandHelpingIcon, ShoppingBagIcon, type LucideIcon } from "lucide-react";
+import { CalendarIcon, CheckIcon, CircleIcon, CoinsIcon, EllipsisVerticalIcon, EuroIcon, HandHelpingIcon, ShoppingBagIcon, type LucideIcon } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn, formatCurrency, formatDate } from "@/lib/utils";
 import type { Transaction, TransactionType } from "@/types/transaction";
@@ -27,7 +28,7 @@ export function TransactionItem({ item }: { item: Transaction }) {
                 option.isPending !== undefined && option.isPending && "border-amber-100/10 bg-linear-to-r from-amber-100/4 to-amber-100/5",
                 option.isPending !== undefined && !option.isPending && "bg-background dark:bg-card/20",
             )}>
-            <CardContent className="grid grid-cols-[min-content_auto_min-content] grid-rows-[min-content_auto] items-start gap-x-4 gap-y-2">
+            <CardContent className="grid grid-cols-[min-content_auto_min-content_min-content] grid-rows-[min-content_auto] items-start gap-x-4 gap-y-2">
                 <div
                     className={cn(
                         "row-span-2 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border",
@@ -35,7 +36,7 @@ export function TransactionItem({ item }: { item: Transaction }) {
                         option.isPending !== undefined && option.isPending && "border-amber-100/20 text-amber-100",
                         option.isPending !== undefined && !option.isPending && "bg-background dark:bg-card text-muted-foreground/40",
                     )}>
-                    <option.Icon className="h-5 w-5" />
+                    <option.Icon className="size-5" />
                 </div>
                 <h3
                     className={cn(
@@ -57,6 +58,11 @@ export function TransactionItem({ item }: { item: Transaction }) {
                     <span className="text-xl font-bold">{integerAmount}</span>
                     <span className="text-sm font-medium">,{decimalAmount}</span>
                 </div>
+                <Button
+                    variant={"ghost"}
+                    size={"icon"}>
+                    <EllipsisVerticalIcon className="size-5" />
+                </Button>
                 <div
                     className={cn(
                         "col-span-2 flex flex-row items-center justify-start gap-2",
